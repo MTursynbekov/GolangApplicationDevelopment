d89365efb4f69b3822f4e4c8ead880ed24b52012
Mubarak Tursynbekov <kuanyshbekuly1002@gmail.com>
2022-03-22 03:59:33 +0600

    hw4 done

---
+++ hw4/ex1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func a(ch chan bool) {
+	for i := 0; i < 50; i++ {
+		fmt.Print("a")
+	}
+	ch <- true
+}
+
+func b(ch chan bool) {
+	for i := 0; i < 50; i++ {
+		fmt.Print("b")
+	}
+	ch <- true
+}
+
+func main() {
+	ch := make(chan bool)
+	go a(ch)
+	go b(ch)
+	_, _ = <-ch, <-ch
+	fmt.Println("\nend main()")
+}
